3db1e0b20fb5ee46fd218865077bb1a098258a97
Tiago Oliveira <tiiagoveira@gmail.com>
2025-02-17 15:44:42 -0300

    ok

--- aula14ex/ex002/script.js
+++ aula14ex/ex002/script.js
@@ -1,1 +1,33 @@
-alert('Olá')
+function tabuada(){
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+
+    if(num.value.length == 0) {
+        alert('Por favor, digite um número')
+    } else {
+        let n = Number(num.value)
+        
+        // variável contadora
+        let c = 1
+        
+        // limpando o campo select antes de executar a função
+        tab.innerHTML = ''
+        
+        while(c <= 10) {
+            // associando opções a variável item
+            let item = document.createElement('option')
+            
+            // formatando a opção como irá aparecer no select
+            item.text = `${n} x ${c} = ${n*c}`
+            
+            // adicionando um valor ao value para saber qual foi o valor selecionado
+            // caso queira fazer algo com esse resultado(opcional)
+            item.value = `tab${c}`
+            
+            // adicionando ao select a opção formatada
+            tab.appendChild(item)
+            
+            c++
+        }
+    }
+}
